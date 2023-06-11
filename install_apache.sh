25b69a4a7269a8d240c5daf4d9bf3468d014f9f7
ayusarin <107953219+ayusarin@users.noreply.github.com>
2023-06-11 16:03:09 +0530

    Update install_apache.sh

--- install_apache.sh
+++ install_apache.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-sudo apt-get -y update
-sudo apt-get -y install apache2
-sudo su -
-echo "Hello World from $(hostname -f)" > /var/www/html/index.html
-sudo systemctl start apache2
-sudo systemctl enable apache2
+sudo apt -y update;
+sudo apt -y install apache2;
+sudo echo "Hello World from $(hostname -f)" > /var/www/html/index.html;
+sudo systemctl start apache2;
+sudo systemctl enable apache2;
