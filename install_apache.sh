58a3ee1fb9d9c765ff6d418b478459f130816c76
ayusarin <107953219+ayusarin@users.noreply.github.com>
2023-06-11 16:46:14 +0530

    Delete install_apache.sh

--- install_apache.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo apt -y update;
-sudo apt -y install apache2;
-sudo echo "Hello World from $(hostname -f)" > /var/www/html/index.html;
-sudo systemctl start apache2;
-sudo systemctl enable apache2;
